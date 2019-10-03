5848c9f01447a03e760414d26d6cee4466619e0c
Facundo Villard <facuvillard@gmail.com>
2019-10-03 16:59:14 -0300

    Initial commit

--- Routes/countries.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(app){
-    var countriesController = require('../Controller/countriesController');
-
-    app.route('/countries').get(countriesController.list_all_countries);
-
-    app.route('/countries/:countryId').get(countriesController.read_a_country);
-};
-
